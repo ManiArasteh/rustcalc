82d4d73534ee7ac6ae0105970e939001f6ce35e9
Mani <mani.arasteh92@gmail.com>
2024-10-12 19:05:26 +0330

    Initial commit🎉

---
+++ README.md
@@ -0,0 +1,3 @@
+# Mathcalculate
+> There's a Mathcalculate for Python. Avaliable on PyPI
+You can use it for calculation.
---
+++ src/lib.rs
@@ -0,0 +1,16 @@
+pub fn add(first: u64, second: u64) -> u64 {
+    first + second
+}
+
+pub fn remove(first: u64, second: u64) -> u64 {
+    first - second
+}
+
+pub fn multiply(first: u64, second: u64) -> u64 {
+    first * second
+}
+
+pub fn divide(first: u64, second: u64) -> u64 {
+    first / second
+}
+
